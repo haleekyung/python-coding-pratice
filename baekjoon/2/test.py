f57d819a87057f6405ddd9d939305e69cc4ef2fa
haleekyung <kyungha03@gmail.com>
2021-07-23 18:23:13 +0900

    upload(not completed)

--- baekjoon/2/test.py
+++
@@ -1,9 +0,0 @@
-testcase = int( input( "테스트케이스 갯수: " ) )
-
-for i in range( testcase ):
-    ox = str( input( "테스트케이스 문제: " ) )
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print( f'Hi, {name}' )  # Press ⌘F8 to toggle the breakpoint.
